a2be5432224036b8bcadee23148cff672c12cf3b
Kirill <m_kirill72@mail.ru>
2023-06-10 08:59:44 +0300

    bild 0.0.1

---
+++ Intermediate/Source/roadGame.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class roadGame : ModuleRules
+{
+	public roadGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
